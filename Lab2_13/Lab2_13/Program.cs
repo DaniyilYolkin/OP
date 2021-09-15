f48acf67aadbabe212a3c50422d33facbd754f91
ІП-12 Йолкін Даніїл <88449864+DaniyilYolkin@users.noreply.github.com>
2021-09-15 13:19:46 +0300

    Lab2_13(Python + C#)

---
+++ Lab2_13/Lab2_13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab2_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsNumberIsInRange(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine())));
+        }
+
+        static bool IsNumberIsInRange(double x, double y)
+        {
+            if ((x >= -1 && x <= 1 && (Math.Pow(x - 1, 2) + Math.Pow(y, 2) <= 4))
+            || (x >= 1 && x <= 3 && x - 3 <= y && - x + 3 >= y)){
+                return true;
+            }
+            return false;
+        }
+    }
+}
