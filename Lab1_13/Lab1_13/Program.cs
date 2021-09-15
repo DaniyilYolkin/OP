a6bef21a38e6730dce3f7be418ed52db3588312a
ІП-12 Йолкін Даніїл <88449864+DaniyilYolkin@users.noreply.github.com>
2021-09-15 13:12:17 +0300

    Lab1_13(Python + C#)

---
+++ Lab1_13/Lab1_13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            var d = Console.ReadLine();
+            var n = Console.ReadLine();
+            double convertedA = Convert.ToDouble(a);
+            double convertedD = Convert.ToDouble(d);
+            int convertedN = Convert.ToInt32(n);
+            if(convertedN <= 0)
+            {
+                throw new Exception("ValueError");
+            }
+            Console.WriteLine("Result is " + FindTheValueOfArithmeticProgression(convertedA, convertedD,convertedN));
+        }
+
+        static double FindTheValueOfArithmeticProgression(double a, double d, int n)
+        {
+            return a + (n - 1) * d;
+        }
+    }
+}
