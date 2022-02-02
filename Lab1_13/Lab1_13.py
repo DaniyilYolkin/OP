942a16926c3f5afe4c9ede9de129af4f3e8187f5
ІП-12 Йолкін Даніїл <88449864+DaniyilYolkin@users.noreply.github.com>
2022-02-02 21:47:20 +0200

    Delete Lab1_13 directory

--- Lab1_13/Lab1_13.py
+++
@@ -1,8 +0,0 @@
-def find_the_value_of_arithmetic_progression(a, d, n):
-    if n > 0:
-        return a+(n-1)*d
-    raise ValueError('n must be greater than 0')
-
-
-print(find_the_value_of_arithmetic_progression(
-    float(input('a: ')), float(input('d: ')), int(input('n: '))))
